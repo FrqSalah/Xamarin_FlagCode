12a45da3b9ccb2d9165d52830252a65002f9087f
Salah <frq.salah@gmail.com>
2017-01-19 17:15:55 +0100

    New Toolbar

--- CountryPhone_Xamarin/MainActivity.cs
+++ CountryPhone_Xamarin/MainActivity.cs
@@ -1,15 +1,18 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
-using System.Collections.Generic;
-using System;
+using Android.Views;
+using Android.Support.V7.App;
+using Android.Support.V4.View;
+using Android.Runtime;
 
 namespace CountryPhone_Xamarin
 {
-    [Activity(Label = "Countries Phone code", MainLauncher = true, Icon = "@drawable/icon")]
-    public class MainActivity : Activity
+    [Activity(Label = "Phone code", MainLauncher = true, Icon = "@drawable/icon", Theme = "@style/Theme.AppCompat.Light")]
+    public class MainActivity : AppCompatActivity
     {
         public CountryData cCoutnries = new CountryData();
+        private SearchView _serachView;
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
@@ -26,14 +29,25 @@ namespace CountryPhone_Xamarin
             // Création de l'adapter pour la listeView
             var adapter = new CountryAdapter(this, cCoutnries.Countries);
             countriesListView.Adapter = adapter;
+        }
 
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Layout.top_menu, menu);
+            var item = menu.FindItem(Resource.Id.Search24);
+
+            var searchView = MenuItemCompat.GetActionView(item);
+            _serachView = searchView.JavaCast<SearchView> ();
+
+            //_serachView.QueryTextChange += (s,e) => 
+            return base.OnCreateOptionsMenu(menu);
         }
 
         private void OnItemClick(object sender, AdapterView.ItemClickEventArgs e)
         {
             var country = cCoutnries.Countries[e.Position];
 
-            var dialog = new AlertDialog.Builder(this);
+            var dialog = new Android.Support.V7.App.AlertDialog.Builder(this);
             dialog.SetMessage(country.cName+"/"+country.cIso+"/"+country.cCodeStr);
             dialog.SetNeutralButton("OK", delegate { });
             dialog.Show();
